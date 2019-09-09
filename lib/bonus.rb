365f25d58162025b897508495fc41175c712dd57
Li Xie <li_xie1992@hotmail.com>
2019-09-09 04:40:05 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,7 +24,8 @@ def bonus
   
 
   epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  
 
   
 
